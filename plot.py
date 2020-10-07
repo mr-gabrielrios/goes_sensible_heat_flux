44115141713025ccef24e0da72216cc83a5ec47c
mr-gabrielrios <mr.gabrielrios@gmail.com>
2020-10-07 12:56:34 -0400

    Addition of functionality to plot parameter value and error data on same plot, addition of functionality to accept multiple entries simultaneously to prevent dependence on one parameter

--- plot.py
+++ plot.py
@@ -3,19 +3,46 @@
 
 ### Imports
 import matplotlib.pyplot as plt
+import numpy as np
 
 ### Main functions
-def main(iter_list, L_list, err_list):
-    plotter(iter_list, L_list, err_list)
+def main(i, dataset_names, *args, **kwargs):
+    plotter(i, dataset_names, *args, **kwargs)
 
 if __name__ == "__main__":
     main()
+
+### Plotter accepts:
+# i (int): number of iterations on outer loop of goes_sensible_heat_flux.py
+# dataset_names (list): list of names for the datasets plotted in args, kwargs
+# *args: list items, typically from the val_dict, to plot values for parameters of interest
+# **kwargs: list items, typically from the error_dict, to plot error values for parameters of interest    
+def plotter(i, dataset_names, *args, **kwargs):
+    # Initialize plot 
+    fig, ax1 = plt.subplots()
     
-def plotter(iter_list, L_list, err_list):
-    # Plot can be updated with variables for each field for easier editing/replacement        
-    plt.plot(iter_list[2:], L_list[2:])
-    plt.plot(iter_list[2:], err_list[2:])
-    plt.title('Convergence statistics')
+    # Plot metadata
+    plt.title('Model run statistics')
     plt.xlabel('Iteration #')
-    plt.ylabel('L (m), Error (%)')
+    
+    # Iterand for legend label selection. Used due to the secondary y-axis
+    n = 0
+    ax1.tick_params(axis='y')  
+    # Loop to plot all value data in *args
+    for arg in args:      
+        ax1.plot(np.linspace(0, i, i), arg, label=dataset_names[n])   
+        n += 1
+        
+    ax2 = ax1.twinx()
+    ax2.tick_params(axis='y')
+    ax2.set_ylabel('Error')
+    # Loop to plot all error value data in **kwargs
+    for val in kwargs.values():
+        ax2.plot(np.linspace(0, i, i), val, label=dataset_names[n])
+        n += 1        
+    
+    # Re-group each y-axis label, combine into one legens
+    lines = ax1.get_lines() + ax2.get_lines()
+    ax1.legend(lines, [line.get_label() for line in lines])
+        
     plt.show()
