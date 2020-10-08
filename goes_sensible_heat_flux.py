b2bec8ad7a604a4ab02ef6c76c05f9fb38e4c7c0
mr-gabrielrios <mr.gabrielrios@gmail.com>
2020-10-08 18:36:19 -0400

    Modified algorithm to mirror Kim et al. (2019). Note the large sensitivity to the initial condition for z_0m.

--- goes_sensible_heat_flux.py
+++ goes_sensible_heat_flux.py
@@ -1,3 +1,5 @@
+### TRIAL 1 - PROGRAM FLOW SIMILAR TO LITERATURE        
+
 ### Objective
 # The objective of this script is to generate the sensible heat flux based on input from GOES data
 
@@ -21,7 +23,7 @@ import time
 rho = 1.225     # Density of air (kg/m^3), assuming p = 1013.25 hPa and T = 15 degC
 c_p = 1006      # Specific heat capacity of air (J/kg-K), assuming p = 1000 hPa and T = 0 degC
 k = 0.4         # von Karman constant
-z_r = 2.2         # Reference height (m), typically 2 m, based on air temperature model per Hrisko et al. (2020)
+z_r = 2.5        # Reference height (m), typically 2 m, based on air temperature model per Hrisko et al. (2020)
                 # 3 m chosen to match with Kim et al. (2019), Case GH1
 h_0 = 2.60      # Vegetation canopy height (m), assumption
 g = 9.81        # Gravitational acceleration constant (m/s^2)
@@ -31,19 +33,20 @@ R = 287.05      # Gas constant for air (J/kg-K)
 ### 2. Dynamic inputs
 ## Station inputs
 p_air = 1013.25 # Atmospheric pressure (hPa), assumption - weather station input
-u_r = 5.8         # Wind speed at reference height (m/s), assumption - weather station input
+u_r = 0.3         # Wind speed at reference height (m/s), assumption - weather station input
 ## Model inputs
-T_lst = 300     # Land surface temperature (LST) (K), assumption - Hrisko model input
-T_air = 291.5     # Air temperature at reference height (K), assumption - Hrisko model input
+T_lst = 303     # Land surface temperature (LST) (K), assumption - Hrisko model input
+T_air = 290    # Air temperature at reference height (K), assumption - Hrisko model input
 
 ### 3. Initial conditions
 L = 100         # Initial condition for Obukhov length
 u_star = 1      # Initial condition for friction velocity
-z_0m = .01       # Initial condition for aerodynamic roughness length
+z_0m = .1       # Initial condition for aerodynamic roughness length
 q_h = 100       # Initial condition for sensible heat flux
 x = 0.1         # Initial condition for parameter that's a function of the Richardson number
 psi_m = 0       # Initial condition for similarity functions for stability
 psi_h = 0
+C_h = 1         # Initial condition for heat transfer coefficient
 
 ### 4. Calculate secondary parameters
 
@@ -72,7 +75,7 @@ val_dict = {'q_h': [q_h],
 
 ## Assume initial values for the errors at 100%
 err_val = 1
-q_h_err, z_0m_err, u_star_err, L_err = [1, 1, 1, 1]
+q_h_err, z_0m_err, u_star_err, C_h_err, L_err = [1, 1, 1, 1, 1]
 ## Create dictionary to store error history in lists for each variable
 err_dict = {'q_h': [q_h_err], 
        'C_h': [err_val],
@@ -84,6 +87,7 @@ err_dict = {'q_h': [q_h_err],
 
 # Set convergence criteria at 1%
 conv_crit = 0.01
+iter_limit = 100
 
 ### 5. Iterative solution for parameters z_0m, q_h, and L
 ## Start timing solution algorithm
@@ -91,161 +95,93 @@ start_time = time.time()
 # Initialize iterand at 1 to allow for a list item with index 0 to be called
 i = 1 
 ## Initialize outer while loop
-while q_h_err > conv_crit:
+while L_err > conv_crit:
     
-    print('Iteration #%d' % i)
-    L_err, z_0m_err = [1, 1]    
+    print('#####################################')
+    print('Iteration %d' % i)
+    print('Stability parameter: %.4f' % ((z_r-d_0)/L))
+    
+    ######
+    print('z_0m: %.4f | z_0m_err: %.4f' % (z_0m, z_0m_err))
+    z_0m_n = (z_r - d_0) / math.exp(0.4*u_r/u_star + psi_m*((z_r-d_0)/L))
+    # z_0m_n = (z_r - d_0) / \
+    #     math.exp(0.4*u_r*(math.log((z_r-d_0)/z_0m) - psi_m*((z_r-d_0)/L))/(k*(u_r-u_0)) + psi_m*((z_r-d_0)/L))          
+    val_dict['z_0m'].append(z_0m_n)
+    z_0m_err = abs((z_0m_n - z_0m)/z_0m)
+    err_dict['z_0m'].append(z_0m_err)
+    ######
+    
     j = 1
-    while (abs(L_err)) > conv_crit:        
+    while q_h_err > conv_crit:
+        print('----------------------------')
+        print('Sub-iteration %d' % j)
         
-        print('Sub-iteration #%d' % j)
-        # Print list of initial sub-iteration values (keep commented except for troubleshooting)
-        print('L: %.4f ' % (L))  
-        print('Stability parameter: %.4f' % ((z_r-d_0)/L))
+        ######
+        print('u_star %.4f | u_star_err: %.4f' % (u_star, u_star_err))
+        u_star_n = (k*(u_r - u_0))/(math.log((z_r-d_0)/z_0m) - psi_m*((z_r - d_0)/L))  
+        u_star_err = abs((u_star_n - u_star)/u_star)  
+        val_dict['u_star'].append(u_star)
+        err_dict['u_star'].append(u_star_err)
+        ######
         
-        ## Constant calculation as a function of Ri, ref. Kim et al. (2019), Eqn. 6
-        x = math.pow((1 - 15*(z_r - d_0)/L), 0.25)
-        ## Alternate calculation of constant as f(Ri), ref. Pearlmutter et al. (2004), Eqn. 4
-        # T = (T_lst-T_air)/2 # Assume temperature is mean of ref. ht. temperature and LST
-        # Ri = g*((T_lst-T_air)/z_r)/(T*math.pow((u_r-u_0)/z_r, 2))
-        # x = math.pow((1 - 16*Ri), 0.25)
+        ######
+        print('C_h %.4f | C_h_err: %.4f' % (C_h, C_h_err))
+        C_h_n = math.pow(k, 2) / ((math.log((z_r-d_0)/z_0m) - psi_m * (z_r-d_0)/L) * \
+                                    (math.log((z_r-d_0)/z_0m) - psi_h * (z_r-d_0)/L))
+        C_h_err = abs((C_h_n - C_h)/C_h)
+        val_dict['C_h'].append(C_h_n)
+        err_dict['C_h'].append(C_h_err)
+        ######
         
-        ## Stability similarity functions, ref. Kim et al. (2019), Eqn. 5, 6 and Garratt, Ch 3.3
-        if (z_r/L) >= 0:
-            psi_m = -5*(z_r - d_0)/L
-            psi_h = psi_m
-        elif (z_r/L) < 0:
-            psi_m = math.log(((1 + math.pow(x,2))/2) * math.pow((1 + x)/2, 2)) \
-                - 2*math.atan(x) + math.pi/2
-            psi_h = 2*math.log((1 + math.pow(x, 2))/2) # Potential source of inaccuracy/error
-           
-        ## Obukhov length calculation, ref. Kim et al. (2019), Eqn. 8
-        # L_n = -rho * c_p * math.pow(u_star, 3) * 0.5*(theta_0 + theta_r) / (k * g * q_h)
-        ## Obukhov length calculation, modified version of Kim et al. (2019), Eqn. 8 by Rios
-        ## Note: this modification removes dependence on u_star to allow for recursive calculation
-        L_n = (0.5*(theta_0+theta_r)*math.pow((u_r-u_0),3)/(g*u_r*(theta_0-theta_r))) * \
-            ((math.log((z_r-d_0)/z_0m)-psi_h*((z_r-d_0)/L)) / \
-             math.pow((math.log((z_r-d_0)/z_0m)-psi_m*((z_r-d_0)/L)), 2))
-                
-        ## Append sub-iteration values
-        val_dict['L'].append(L_n)     
+        ######
+        print('q_h %.4f | q_h_err: %.4f' % (q_h, q_h_err))
+        q_h_n = rho * c_p * C_h * u_r * (theta_0 - theta_r)
+        q_h_err = abs((q_h_n - q_h)/q_h)
+        val_dict['C_h'].append(q_h_n)
+        err_dict['C_h'].append(q_h_err)
+        ######
         
-        # Print list of final sub-iteration values (keep commented except for troubleshooting)
-        # print('L_n: %.4f' % (L_n))   
-        
-        # Calculate and print L error
-        # L_err = abs((val_dict['L'][j] - val_dict['L'][j-1])/val_dict['L'][j-1])
-        L_err = abs((L_n - L)/L)
-        err_dict['L'].append(L_err)
-        
-        print('L error: %.4f' % (err_dict['L'][j]))
-        
-        # Reset parameters to new values
-        L = L_n
-        
-        # Limit nested loop to 20 iterations
-        if j < 20:
+        # Cold break to prevent run-on solution
+        if j < iter_limit:
+            u_star = u_star_n
+            C_h = C_h_n
+            q_h = q_h_n
             j += 1
         else:
-            break
-    print('#######################')
-    ### Inner loop to simultaneously solve z_0m and L    
-    # Initialize iterand at 1 to allow for a list item with index 0 to be called
-    m = 1 # Nested loop iterand   
-    ## Initialize outer while loop
-    while (abs(z_0m_err)) > conv_crit:        
+            break 
+    
+    ######        
+    x = math.pow((1 - 15*(z_r - d_0)/L), 0.25)
+    
+    if (z_r/L) >= 0:
+        psi_m = -5*(z_r - d_0)/L
+        psi_h = psi_m
+    elif (z_r/L) < 0:
+        psi_m = math.log(((1 + math.pow(x,2))/2) * math.pow((1 + x)/2, 2)) \
+            - 2*math.atan(x) + math.pi/2
+        psi_h = 2*math.log((1 + math.pow(x, 2))/2)
+    ######
         
-        print('Sub-iteration #%d' % m)
-        # Print list of initial sub-iteration values (keep commented except for troubleshooting)
-        print('z_0m: %.4f ' % (z_0m))  
-        print('Stability parameter: %.4f' % ((z_r-d_0)/L))
-        
-        ## Aerodynamic roughness length, ref. Kim et al. (2019), Eqn. 4 and 
-        ## Brummer et al. (2002), Eqn. 1
-        # u_star = friction velocity (m/s)
-        # z_0m_n = (z_r - d_0) / math.exp((0.4*u_r/u_star) + psi_m*((z_r-d_0)/L))  
-        ## Aerodynamic roughness length, modified version of Kim et al. (2019) and Brummer et al. (2002) by Rios
-        ## Note: this modification removes dependence on u_star to allow for recursive calculation
-        
-        # Print components to diagnose z_0m
-        # print('Denominator: ')
-        # print('[0.4 * %.3f * ln(%.3f/%.3f)-%.3f*(%.3f / %.4f)] / %.3f*(%.3f - %.3f) + %.3f*(%.3f / %.4f) = %.8f' \
-        #       % (u_r, z_r-d_0, z_0m, psi_m, z_r-d_0, L, k, u_r, u_0, psi_m, z_r-d_0, L, \
-        #          0.4*u_r*(math.log((z_r-d_0)/z_0m) - psi_m*((z_r-d_0)/L))/(k*(u_r-u_0)) + psi_m*((z_r-d_0)/L)))
-        
-        z_0m_n = (z_r - d_0) / \
-            math.exp(0.4*u_r*(math.log((z_r-d_0)/z_0m) - psi_m*((z_r-d_0)/L))/(k*(u_r-u_0)) + psi_m*((z_r-d_0)/L))           
-                
-        ## Append sub-iteration values
-        val_dict['z_0m'].append(z_0m_n)
-        
-        # Print list of final sub-iteration values (keep commented except for troubleshooting)
-        # print('z_0m_n: %.4f' % (z_0m_n))   
-        
-        # Calculate and print z_0m error
-        # z_0m_err = abs((val_dict['z_0m'][j] - val_dict['z_0m'][j-1])/val_dict['z_0m'][j-1])
-        z_0m_err = abs((z_0m_n - z_0m)/z_0m)
-        err_dict['z_0m'].append(z_0m_err) 
-        
-        print('z_0m error: %.4f' % (err_dict['z_0m'][m]))
-        
-        # Reset parameters to new values
+    ######    
+    print('L: %.4f | L_err: %.4f' % (L, L_err))
+    L_n = -rho * c_p * math.pow(u_star, 3) * 0.5*(theta_0 + theta_r) / (k * g * q_h)
+    # L_n = (0.5*(theta_0+theta_r)*math.pow((u_r-u_0),3)/(g*u_r*(theta_0-theta_r))) * \
+    #     ((math.log((z_r-d_0)/z_0m)-psi_h*((z_r-d_0)/L)) / \
+    #      math.pow((math.log((z_r-d_0)/z_0m)-psi_m*((z_r-d_0)/L)), 2)) 
+    L_err = abs((L_n - L)/L)
+    val_dict['L'].append(L_n)   
+    err_dict['L'].append(L_err) 
+    ######
+    
+    # Cold break to prevent run-on solution
+    if i < iter_limit:
         z_0m = z_0m_n
-        
-        # Limit nested loop to 20 iterations
-        if m < 20:
-            m += 1
-        else:
-            break
-    
-    # Print lists for the inner loop values
-    # print('### Values (z_0m - 1st line, L - 2nd line) ###')
-    # print(*val_dict['z_0m'], sep=', ')
-    # print(*val_dict['L'], sep=', ')
-    # print('### Errors (z_0m - 1st line, L - 2nd line) ###')
-    # print(*err_dict['z_0m'], sep=', ')
-    # print(*err_dict['L'], sep=', ')
-    
-    # Print final z_0m value
-    # print('z_0m = %.4f | L = %.4f' % (z_0m, L))    
-    # break # This serves to isolate the inner loop for troubleshooting
-    
-    ## Friction velocity calculation, ref. Kim et al. (2019), Eqn. 7
-    # u_0 =  wind speed at surface (m/s), assumption
-    u_star = (k*(u_r - u_0))/(math.log((z_r-d_0)/z_0m) - psi_m*((z_r - d_0)/L))    
-    val_dict['u_star'].append(u_star)
-    err_dict['u_star'].append(abs((val_dict['u_star'][i] - val_dict['u_star'][i-1])/val_dict['u_star'][i-1]))
-
-    ## Exchange coefficient calculation, ref. Kim et al. (2019), Eqn. 2
-    # d_0 = zero-plane displacement height (m)
-    # z_0m = aerodynamic roughness length (m)
-    # psi_m = atmospheric stability parameter for momentum
-    # psi_h = atmospheric stability parameter for sensible heat flux
-    # L = Obukhov length (m)
-    C_h = math.pow(k, 2) / ((math.log((z_r-d_0)/z_0m) - psi_m * (z_r-d_0)/L) * \
-                            (math.log((z_r-d_0)/z_0m) - psi_h * (z_r-d_0)/L))
-        
-    ## Sensible heat flux calculation, ref. Kim et al. (2019), Eqn. 1
-    # q_h = sensible heat flux (W/m^2)
-    # C_h = sensible heat exchange coefficient
-    # u_r = wind speed at reference height (m/s)
-    # theta_0 = potential temperature at surface (K)
-    # theta_r = potential temperature at reference height (K) 
-    q_h = rho * c_p * C_h * u_r * (theta_0 - theta_r)
-    val_dict['q_h'].append(q_h)        
-    err_dict['q_h'].append(q_h_err)
-    q_h_err = abs((val_dict['q_h'][i] - val_dict['q_h'][i-1])/val_dict['q_h'][i-1])
-    
-    print('q_h error: %.4f' % q_h_err)
-    print('\n')
-    
-    # Cold break at i = 10 to prevent run-on solution
-    if i > 20:
+        L = L_n
         i += 1
-        break
+        print('\n')
     else:
-        i += 1    
-    
+        break   
+   
 ## Calculate time elapsed for iterative solution
 elapsed_time = time.time() - start_time
 
@@ -254,13 +190,12 @@ def main():
     ### Print statements
     print('Sensible heat flux for selected pixel is: %.2f W/m^2' % q_h)
     print('Obukhov length for the selected pixel is: %.2f m' % L)
-    print('Time elapsed: %.4f s' % elapsed_time)    
-    # Outer loop parameter plot
-    plot.plotter(i, ['Q_h (W/m^2)', 'Q_h error'], val_dict['q_h'], err1=err_dict['q_h'])
-    # Inner loop parameter plot
-    plot.plotter(len(val_dict['L']), ['L (m)', 'L error'], val_dict['L'], err1=err_dict['L'])
-    # Inner loop parameter plot
-    plot.plotter(len(val_dict['z_0m']), ['z_0m (m)', 'z_0m error'], val_dict['z_0m'], err1=err_dict['z_0m'])
+    print('Roughness length for the selected pixel is: %.2f m' % z_0m)
+    print('Time elapsed: %.4f s' % elapsed_time)   
+    
+    # plot.plotter(len(val_dict['q_h']), ['Q_h (W/m^2)', 'Q_h error'], val_dict['q_h'], err1=err_dict['q_h'])    
+    # plot.plotter(len(val_dict['L']), ['L (m)', 'L error'], val_dict['L'], err1=err_dict['L'])    
+    # plot.plotter(len(val_dict['z_0m']), ['z_0m (m)', 'z_0m error'], val_dict['z_0m'], err1=err_dict['z_0m'])
 
 if __name__ == "__main__":
-    main()
+    main()  
